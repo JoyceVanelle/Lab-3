b5eb3a2669e2bdf0331ac9b3fc551255c8243ebf
francine265 <fmatho@yahoo.com>
2022-11-24 12:35:10 -0500

    finalisation classe Projet

--- laboratoire 3/laboratoire 3/Projet.cs
+++ laboratoire 3/laboratoire 3/Projet.cs
@@ -8,5 +8,40 @@ namespace laboratoire_3
 {
     internal class Projet
     {
+
+        String numero;
+        DateOnly debut;
+        int budget;
+        String description;
+        String employe;
+        public Projet()
+        {
+            this.Numero = "";
+            this.Debut = new DateOnly();
+            this.Budget = 0;
+            this.Description = "";
+            this.Employe = "";
+        }
+        public Projet(string numero, int _adebut, int _moisdebut, int _jourdebut, int budget, string description, string employe)
+        {
+            this.Numero = numero;
+            this.Debut = new DateOnly(_adebut, _moisdebut, _jourdebut);
+            this.Budget = budget;
+            this.Description = description;
+            this.Employe = employe;
+        }
+
+        public string Numero { get => numero; set => numero = value; }
+        public DateOnly Debut { get => debut; set => debut = value; }
+        public int Budget { get => budget; set => budget = value; }
+        public string Description { get => description; set => description = value; }
+        public string Employe { get => employe; set => employe = value; }
+
+        public override string ToString()
+        {
+            return numero + " " + debut.ToString() + " " + budget + " " + description + " " + employe;
+        }
+
+
     }
 }
