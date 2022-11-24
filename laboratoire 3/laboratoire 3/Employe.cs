74e6a13ea28d5c2b000f269f9afd5565cd9940d3
JoyceVanelle <tsatiamomo@gmail.com>
2022-11-24 13:12:42 -0500

    modif de la class Employe

--- laboratoire 3/laboratoire 3/Employe.cs
+++ laboratoire 3/laboratoire 3/Employe.cs
@@ -8,5 +8,32 @@ namespace laboratoire_3
 {
     internal class Employe
     {
+
+        String matricule;
+        String nom;
+        String prenom;
+
+        public Employe()
+        {
+            this.Matricule = "";
+            this.Nom = "";
+            this.Prenom = "";
+        }
+
+        public Employe(string _matricule, string _nom, string _prenom)
+        {
+            this.Matricule = _matricule;
+            this.Nom = _nom;
+            this.Prenom = _prenom;
+        }
+
+        public string Matricule { get => matricule; set => matricule = value; }
+        public string Nom { get => nom; set => nom = value; }
+        public string Prenom { get => prenom; set => prenom = value; }
+
+        public override string ToString()
+        {
+            return matricule + " " + nom + " " + prenom;
+        }
     }
 }
