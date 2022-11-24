ebe40b65169def997230331cd8e1f5105f616ca3
francine265 <fmatho@yahoo.com>
2022-11-24 15:36:09 -0500

    ajout de la methode Afficher dans Gestion BD

--- laboratoire 3/laboratoire 3/GestionBD.cs
+++ laboratoire 3/laboratoire 3/GestionBD.cs
@@ -7,6 +7,7 @@ using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static Org.BouncyCastle.Bcpg.Attr.ImageAttrib;
 
 namespace laboratoire_3
 {
@@ -27,6 +28,52 @@ namespace laboratoire_3
                 gestionBD = new GestionBD();
 
             return gestionBD;
+        }
+        public ObservableCollection<Projet> GetProjet()
+        {
+            try
+            {
+                liste.Clear();// pour vider la liste
+
+                MySqlCommand commande = new MySqlCommand();
+                commande.Connection = con;// indique le chemin à commande 
+                commande.CommandText = "Select * from Projet";// ce qu,il faut aller chercher
+
+                con.Open();// ouvre la connection 
+                MySqlDataReader r = commande.ExecuteReader();// permet de lire le retour qui ewst stocké dans r
+                r.Read();
+             //   public static bool TryParseExact(string? s, string? format, out DateOnly result);
+
+                while (r.Read())
+                   
+                {
+                    liste.Add(new Projet()
+                    {
+                        Numero = r.GetString(0),
+                        Debut = (DateOnly)r["debut"],
+                        Budget =r.GetInt32(2),
+                        Description =r.GetString(3),
+                        Employe = r.GetString(4),
+
+                    });
+
+                    
+                }
+
+
+                r.Close();
+                con.Close();
+            }
+
+              
+
+           
+            catch (MySqlException ex)
+            {
+                if (con.State == System.Data.ConnectionState.Open)
+                    con.Close();
+            }
+            return liste;
         }
     }
 }
