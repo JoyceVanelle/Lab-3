0e6cb482bb25dc27c234fd0d789bfaac43b4ab08
JoyceVanelle <tsatiamomo@gmail.com>
2022-11-24 14:31:24 -0500

    modif singleton

--- laboratoire 3/laboratoire 3/GestionBD.cs
+++ laboratoire 3/laboratoire 3/GestionBD.cs
@@ -12,12 +12,12 @@ namespace laboratoire_3
     {
 
         MySqlConnection con;
-        ObservableCollection<Projet> liste;
+        ObservableCollection<Employe> liste;
         static GestionBD gestionBD = null;
         public GestionBD()
         {
             con = new MySqlConnection("Server=cours.cegep3r.info;Database=a2022_420326ri_gr2;Uid=2014985;Pwd=2014985;");
-            liste = new ObservableCollection<Projet>();
+            liste = new ObservableCollection<Employe>();
 
         }
         public static GestionBD getInstance()// rapport avec le singleton
@@ -27,5 +27,36 @@ namespace laboratoire_3
 
             return gestionBD;
         }
+
+        public void AjouterEmployer(string matricule, string nom, string prenom)
+        {
+
+            try
+            {
+                MySqlCommand commande = new MySqlCommand();
+                commande.Connection = con;
+                /// commande.CommandText = "insert into clients values(10,'doe','john','mail@mail.com')";
+                commande.CommandText = "insert into employe values( @matricule, @nom, @prenom) ";
+
+                //commande.Parameters.AddWithValue("@id", id);
+                commande.Parameters.AddWithValue("@matricule", matricule);
+                commande.Parameters.AddWithValue("@nom", nom);
+                commande.Parameters.AddWithValue("@prenom", prenom);
+
+                con.Open();
+                commande.Prepare();
+                int i = commande.ExecuteNonQuery();
+
+                con.Close();
+
+            }
+            catch (MySqlException ex)
+            {
+                con.Close();
+            }
+
+
+        }
+
     }
 }
