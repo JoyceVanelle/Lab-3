f2e345151b051ac92e83c27112ddf74c74935c23
francine265 <fmatho@yahoo.com>
2022-11-24 12:09:32 -0500

    creation du singleton,de la classe Projet et de la classe employe

---
+++ laboratoire 3/laboratoire 3/GestionBD.cs
@@ -0,0 +1,32 @@
+﻿using MySql.Data.MySqlClient;
+using Org.BouncyCastle.Asn1.Cms;
+using Org.BouncyCastle.Asn1.X509;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace laboratoire_3
+{
+    internal class GestionBD
+    {
+        MySqlConnection con;
+        ObservableCollection<Projet> liste;// pas automatique , on le fait car on fera un select de liste plustard
+        static GestionBD gestionBD = null;// ce qui crée le singleton pour avoir un seul objet à utiliser
+        public GestionBD()
+        {
+            con = new MySqlConnection("Server=cours.cegep3r.info;Database=a2022_420326ri_gr2;Uid=2014985;Pwd=2014985;");
+            liste = new ObservableCollection<Projet>();
+
+        }
+        public static GestionBD getInstance()// rapport avec le singleton
+        {
+            if (gestionBD == null)
+                gestionBD = new GestionBD();
+
+            return gestionBD;
+        }
+    }
+}
