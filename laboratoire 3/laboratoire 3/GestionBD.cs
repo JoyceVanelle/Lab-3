0b0be96c1dd3bf820128c4bebb3bef0e9da11651
francine265 <fmatho@yahoo.com>
2022-11-30 12:48:20 -0500

    ok

--- laboratoire 3/laboratoire 3/GestionBD.cs
+++ laboratoire 3/laboratoire 3/GestionBD.cs
@@ -13,11 +13,13 @@ namespace laboratoire_3
 {
     internal class GestionBD
     {
-
         MySqlConnection con;
+        ObservableCollection<Projet> liste;// pas automatique , on le fait car on fera un select de liste plustard
+        static GestionBD gestionBD = null;// ce qui crée le singleton pour avoir un seul objet à utiliser
         public GestionBD()
         {
             con = new MySqlConnection("Server=cours.cegep3r.info;Database=a2022_420326ri_gr2_2014985-sorelle-francine-matho-ngoualadjo;Uid=2014985;Pwd=2014985;");
+            liste = new ObservableCollection<Projet>();
 
         }
         public static GestionBD getInstance()// rapport avec le singleton
@@ -27,8 +29,8 @@ namespace laboratoire_3
 
             return gestionBD;
         }
+        public ObservableCollection<Projet> GetProjet()
         {
-
             try
             {
 
@@ -39,17 +41,26 @@ namespace laboratoire_3
 
                 
                 {
-                con.Close();
-            }
 
                     DateTime d = r.GetDateTime("debut");
 
+                    d.AddYears(d.Year);
+                    d.AddMonths(d.Month);
+                    d.AddDays(d.Day);
+                    /*
+                                        DateOnly d = new DateOnly();
+                                        DateTime dateTime = r.GetDateTime("debut");
 
+                                        d.AddYears(dateTime.Year);
+                                        d.AddMonths(dateTime.Month);
+                                        d.AddDays(dateTime.Day);
+                    */
+                    liste.Add(new Projet()
                     {
                         Numero = r.GetString(0),
                         Debut = d,
-                        Budget =r.GetInt32(2),
-                        Description =r.GetString(3),
+                        Budget = r.GetInt32(2),
+                        Description = r.GetString(3),
                         Employe = r.GetString(4),
                         NomEmploye = r.GetString(5),
                         PrenomEmploye = r.GetString(6),
@@ -62,23 +73,38 @@ namespace laboratoire_3
 
                 r.Close();
                 con.Close();
-
             }
 
-              
 
-           
+
+
             catch (MySqlException ex)
             {
                 if (con.State == System.Data.ConnectionState.Open)
                     con.Close();
             }
+            return liste;
         }
+        public ObservableCollection<Projet> RechercheProjet(DateTime debut)
         {
             liste.Clear();
             try
             {
-              
+
+
+                MySqlCommand commande = new MySqlCommand("Rechercher_Projet");
+                commande.Connection = con;// indique le chemin à commande 
+                commande.CommandType = System.Data.CommandType.StoredProcedure;
+
+                commande.Parameters.AddWithValue("@datedeb", debut.ToString("yyyy-MM-dd"));// met l'id dans l'espace qui lui a été réservé
+
+
+
+
+                con.Open();// ouvre la connection 
+                //commande.Prepare();// empêche les caractères spéciaux donc prends tout ca comme chaine de caractères
+                int i = commande.ExecuteNonQuery();
+                MySqlDataReader r = commande.ExecuteReader();// permet de lire le retour qui ewst stocké dans r
 
                 
                 while (r.Read())
@@ -97,7 +123,6 @@ namespace laboratoire_3
 
                 r.Close();
                 con.Close();
-
             }
 
             catch (MySqlException ex)
@@ -106,9 +131,8 @@ namespace laboratoire_3
                     con.Close();
             }
             return liste;
-
         }
 
-    
+
     }
 }
